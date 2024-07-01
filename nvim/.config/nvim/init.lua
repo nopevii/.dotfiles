421cf60299737909b1e11256a8df849f86d0006a
nopevii <173698364+nopevii@users.noreply.github.com>
2024-07-01 21:46:48 +0200

    feat: add snippets to neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -108,7 +108,7 @@ require('lazy').setup {
         dependencies = {
             'nvim-lua/plenary.nvim',
             { 'nvim-telescope/telescope-ui-select.nvim' },
-            { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+            { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
             {
                 'nvim-telescope/telescope-fzf-native.nvim',
                 build = 'make',
@@ -201,6 +201,7 @@ require('lazy').setup {
                 'jsonls',
                 'tsserver',
                 'tailwindcss',
+                'templ',
             })
 
             require('mason-tool-installer').setup {
@@ -225,11 +226,37 @@ require('lazy').setup {
         dependencies = {
             'hrsh7th/cmp-nvim-lsp',
             'hrsh7th/cmp-path',
+            'saadparwaiz1/cmp_luasnip',
+            {
+                'L3MON4D3/LuaSnip',
+                build = (function()
+                    if vim.fn.has 'win32' == 1 or vim.fn.executable 'make' == 0 then
+                        return
+                    end
+                    return 'make install_jsregexp'
+                end)(),
+                dependencies = {
+                    {
+                        'rafamadriz/friendly-snippets',
+                        config = function()
+                            require('luasnip.loaders.from_vscode').lazy_load()
+                        end,
+                    },
+                },
+            },
         },
         config = function()
             local cmp = require('cmp')
+            local luasnip = require('luasnip')
+
+            luasnip.config.setup {}
 
             cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
                 completion = {
                     completeopt = 'menu,menuone,noinsert',
                 },
@@ -243,9 +270,20 @@ require('lazy').setup {
 
                     ['<C-Space>'] = cmp.mapping.complete {},
 
+                    ['<C-l>'] = cmp.mapping(function()
+                        if luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-h>'] = cmp.mapping(function()
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        end
+                    end, { 'i', 's' }),
                 },
                 sources = {
                     { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
                     { name = 'path' },
                 },
             }
